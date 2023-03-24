e5c8c1746156b371ce2c67a3fe77fcc09a33d68f
Hiansdt <vinijao123690@gmail.com>
2023-03-24 15:20:23 -0300

    organização pastas

---
+++ dev _web2/exercicio-1/src/App.vue
@@ -0,0 +1,35 @@
+<script setup>
+import { ref, onMounted } from 'vue'
+
+const contador = ref(0)
+
+function incrementarContador() {
+  contador.value++
+}
+function decrementarContador() {
+  if (contador.value > 0) {
+    contador.value-- 
+  }
+}
+function zerarContador() {
+  contador.value = 0
+}
+
+
+onMounted(() => {
+  console.log(`O valor inicial do contador é ${contador.value}.`)
+})
+</script>
+
+<template>
+  <button @click="incrementarContador">Incrementar</button>
+  <button @click="decrementarContador">Decrementar</button>
+  <button @click="zerarContador">Zerar</button>
+  <p>Valor do contador é: {{ contador }}</p>
+</template>
+
+<style scoped>
+button {
+  font-weight: bold;
+}
+</style>
