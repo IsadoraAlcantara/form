244b3941448979c8350843464347a16bef9b49e2
Hiansdt <vinijao123690@gmail.com>
2023-03-24 15:09:26 -0300

    adiciona bootstrap

--- src/App.vue
+++ src/App.vue
@@ -1,65 +1,39 @@
 <script setup>
-import { ref } from'vue'
-    const nome = 'Isadora'
-    const idade = 15
-    const contador = ref(0)
+import { ref, computed } from 'vue'
 
-    function inverter(texto) {
-        return texto.split('').reverse().join('')
-    }
+const contador1 = ref(0)
+const contador2 = ref(0)
+const soma = computed(() => contador1.value + contador2.value)
+/* function soma() {
+  return contador1.value + contador2.value
+} */
+const mostrar = ref(true)
 
-    function inverterMaiusculo(texto) {
-        return texto.split('').reverse().join('').toUpperCase()
-    }
-
-    function saudacao() {
-        return `Olá, ${nome}!`
-    }
 </script>
 
 <template>
-    <div class="info">
-        <h1>Exemplo 1 - Mostrando informações na tela</h1>
-        <p>Nome: {{ nome }} </p>
-        <p>Idade: {{ idade }}  anos</p>
-        <hr />
-        <p>Para completar 50 anos faltam: {{ 50 - idade }}  anos</p>
-        <p>O nome tem {{ nome.length }}  caracteres</p>
-        <p>O nome invertido é: {{ inverterMaiusculo(nome) }} </p>
-        <hr />
-        <p> Exemplo de saudação usando função</p>
-        <p> {{ saudacao() }}  </p>
+  <div class="container">
+    <button class="btn btn-primary" @click="mostrar = !mostrar">Mostrar / Esconder</button>
+    <div v-if="mostrar">
+      <div>
+        <button class="btn btn-primary" @click="contador1--">-</button>
+        <span class="mx-3 fs-4">{{ contador1 }}</span>
+        <button class="btn btn-primary" @click="contador1++">+</button>
+      </div>
+      <div>
+        <button class="btn btn-primary" @click="contador2--">-</button>
+        <span class="mx-3 fs-4">{{ contador2 }}</span>
+        <button class="btn btn-primary" @click="contador2++">+</button>
+      </div>
+      <div>
+        <p class="fs-3">A soma é {{  soma  }}</p>
+        <p v-if="soma > 10" class="fs-3">A soma é maior que 10</p>
+        <p v-else class="fs-3">A soma é menor ou igual a 10</p>
+      </div>
+      <p v-if="soma % 2 === 0" class="fs-3">A soma é par</p>
+      <p v-else class="fs-3">A soma é ímpar</p>
     </div>
-    <div class="info">
-      Contador: {{ contador }}
-      <button @click="contador++">+</button>
-      <button @click="contador--">-</button>
-      <div v-if="contador > 10" class="vermelho">Maior que 10</div>
-      <div v-else-if="contador < 10" class="verde">Menor que 10</div>
-      <div v-else>É 10</div>
-      <div v-show="contador % 2 == 0">Par</div>
-    </div>
+  </div>
 </template>
 
-<style scoped>
-.info {
-  background-color: rgba(35,12,22,1);
-  padding: 20px 30px;
-  color: rgb(183, 210, 219);
-  border-radius: 10px;
-  margin: 0 15px;
-}
-
-h1 {
-  color: white;
-  font-weight: bold;
-  font-size: 1.5em;
-}
-
-.vermelho {
-  color: red;
-}
-.verde {
-  color: green;
-}
-</style>
+<style scoped></style>
